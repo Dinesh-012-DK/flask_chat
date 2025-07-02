17f41b97ad297f90480f37942ce37a64d45154dd
Dinesh-012-DK <dineshkumarmani012@gmail.com>
2025-07-02 10:54:40 -0700

    start spell

--- chat.py
+++ chat.py
@@ -1,9 +1,10 @@
 #!/bin/env python
 from app import create_app, socketio
 import eventlet
-eventlet.monkey_patch
+
+eventlet.monkey_patch()
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=100000)
+    socketio.run(app, host='0.0.0.0', port=10000)
